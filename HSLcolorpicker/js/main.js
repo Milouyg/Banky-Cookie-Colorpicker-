24ea69d7446fdf34fb86ffd9c210194844a7610d
Milouyg <33333@ma-web.nl>
2023-02-16 15:11:47 +0100

    finished assignment without OOP

--- HSLcolorpicker/js/main.js
+++ HSLcolorpicker/js/main.js
@@ -2,15 +2,19 @@ const colors = document.getElementsByClassName("colors__color")
 let secondColor = false;
 for (let i = 0; i < colors.length; i++){
     // set color
-    if (secondColor === false) {
-        colors[i].children[0].style.background = "#6096B4";
-        secondColor = true;
-    }
-    else {
-        colors[i].children[0].style.background = "#9096B4";
-        secondColor = false;
-    }
-    
+
+    // nummer inclusief 1 - 360 inclusief => hue
+    // percentage inclusief 11 - 79 => saturatie
+    // percentage inclusief 11 - 100 => licht
+    // x = Math.random() * (max - min) + min;
+
+    // colors[i].style.animationDelay = i / 10 + "s";
+    let randomHue = Math.floor(Math.random() * (360 - 1) + 1);
+    let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
+    let randomLightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
+
+    colors[i].children[0].style.background = `hsl(${randomHue} ${randomSaturation} ${randomLightness})`;
+
     // onclick
     colors[i].onclick = function () {
         colors[i].children[0].classList.add("colors__circle--selected");
