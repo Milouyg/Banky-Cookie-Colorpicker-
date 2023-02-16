6f719f7a22b8d3f07c9062777efe46c9fa2fa3ba
Milouyg <33333@ma-web.nl>
2023-02-16 14:19:26 +0100

    2 different colors

--- HSLcolorpicker/js/main.js
+++ HSLcolorpicker/js/main.js
@@ -1,5 +1,21 @@
-const color = document.getElementById("js--color--1");
-color.onclick = function () {
-    color.children[0].classList.add("colors__circle--selected");
+const colors = document.getElementsByClassName("colors__color")
+let secondColor = false;
+for (let i = 0; i < colors.length; i++){
+    // set color
+    if (secondColor === false) {
+        colors[i].children[0].style.background = "#6096B4";
+        secondColor = true;
+    }
+    else {
+        colors[i].children[0].style.background = "#9096B4";
+        secondColor = false;
+    }
+    
+    // onclick
+    colors[i].onclick = function () {
+        colors[i].children[0].classList.add("colors__circle--selected");
+        navigator.clipboard.writeText(colors[i].children[0].style.background);
+        document.title = colors[i].children[0].style.background;
+    }
 }
 
